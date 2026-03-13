e2415d221835e865dc7dae8e4dc1b45f1fce659b
Kevin Parks <kev@Kevins-MacBook-Pro.local>
2026-03-13 14:17:02 +0000

    login security added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#0f1724" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
